76401445ec5a3553fffedbb705b4820f772fbeda
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-06-12 17:40:01 +0300

    Adding Plausible analytic.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import PlausibleProvider from "next-plausible";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider domain="liveinestonia.com" trackOutboundLinks={true}>
+      <Component {...pageProps} />
+    </PlausibleProvider>
+  );
 }
 
 export default MyApp;
