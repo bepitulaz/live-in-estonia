c7e890438de6c9bca20e6f3208818f2abdca6bdb
Asep Bagja Priandana <bepitulaz@gmail.com>
2022-06-16 22:10:44 +0300

    Use layout responsive for the photo card.

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -4,19 +4,20 @@ import Link from "next/link";
 
 export default function PhotoCard({ post }) {
   return (
-      <Card className="card-custom">
-        <Card.Body className="p-0">
-          <Link href={post.detailHref} passHref>
-            <a title="Open the photo detail">
-              <Image
-                src={post.imageUrl}
-                alt={post.altText}
-                width={post.width / 2}
-                height={post.height / 2}
-              />
-            </a>
-          </Link>
-        </Card.Body>
-      </Card>
+    <Card className="card-custom">
+      <Card.Body className="p-0">
+        <Link href={post.detailHref} passHref>
+          <a title="Open the photo detail">
+            <Image
+              src={post.imageUrl}
+              alt={post.altText}
+              width={post.width / 2}
+              height={post.height / 2}
+              layout="responsive"
+            />
+          </a>
+        </Link>
+      </Card.Body>
+    </Card>
   );
 }
